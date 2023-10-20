679378177a27962af677c96fdef97732c850c045
pedrober31 <78823560+pedrober31@users.noreply.github.com>
2023-10-19 21:11:12 -0300

    Functions correction

--- python_sertao/functions.py
+++ python_sertao/functions.py
@@ -1,6 +1,7 @@
+import pandas as pd
 import geopandas as gpd
 from shapely import Point
-import pandas as pd
+import matplotlib.pyplot as plt
 
 def compatibility_Coords(filepath, encoding, filename, crs):
     """
@@ -52,50 +53,13 @@ def annual_accum(filename):
 
     annual_accum = data['Precipitação'].resample('Y').sum()
     annual_accum.index = annual_accum.index.year
+    annual_accum.index.name = 'Ano'
 
     return annual_accum
 
 # Example
-# delmiro = "D:\PIBIC 23-24\Estacoes\Estacoes_SIGA\DELMIRO GOUVÉIA.xlsx"
-# aa_delmiro = annual_accum(delmiro)
-
-def month_accum(filename):
-    """
-    Returns monthly accumulated rainfall.
-    """
-    data = pd.read_excel(filename, header=6)
-
-    del data['Unnamed: 0']
-    data.index = pd.to_datetime(data['Data'])
-    del data['Data']
-
-    month_accum = data['Precipitação'].resample('M').sum()
-    month_accum.index = month_accum.index.strftime('%Y-%m')
-
-    return month_accum
-
-# Example
-# delmiro = "D:\PIBIC 23-24\Estacoes\Estacoes_SIGA\DELMIRO GOUVÉIA.xlsx"
-# ma_delmiro = month_accum(delmiro)
-
-def annual_mean(filename):
-    """
-    Returns annual mean rainfall.
-    """
-    data = pd.read_excel(filename, header=6)
-
-    del data['Unnamed: 0']
-    data.index = pd.to_datetime(data['Data'])
-    del data['Data']
-
-    annual_mean = data['Precipitação'].resample('Y').mean()
-    annual_mean.index = annual_mean.index.year
-
-    return annual_mean
-
-# Example
-# delmiro = "D:\PIBIC 23-24\Estacoes\Estacoes_SIGA\DELMIRO GOUVÉIA.xlsx"
-# am_delmiro = annual_mean(delmiro)
+delmiro = "D:\Pesquisa\PIBIC 23-24\Estacoes\Estacoes_SIGA\Filtro_GPM\DELMIRO GOUVÉIA.xlsx"
+aa_delmiro = annual_accum(delmiro)
 
 def month_mean(filename):
     """
@@ -107,11 +71,29 @@ def month_mean(filename):
     data.index = pd.to_datetime(data['Data'])
     del data['Data']
 
-    month_mean = data['Precipitação'].resample('M').mean()
-    month_mean.index = month_mean.index.strftime('%Y-%m')
+    month_mean = data.resample('M').sum()
+    month_mean = month_mean.groupby(month_mean.index.month).mean()
 
     return month_mean
 
 # Example
-# delmiro = "D:\PIBIC 23-24\Estacoes\Estacoes_SIGA\DELMIRO GOUVÉIA.xlsx"
-# mm_delmiro = month_mean(delmiro)
+mm_delmiro = month_mean(delmiro)
+
+# Plotagem
+station = 'Delmiro Gouveia'
+n_rows = 1
+n_cols = 2
+fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols)
+
+aa_delmiro.plot(ax=axes[0], kind='bar', color='blue', edgecolor='black', alpha=0.7)
+mm_delmiro.plot(ax=axes[1], kind='bar', legend=False, color='blue', edgecolor='black', alpha=0.7)
+
+axes[0].set_ylabel('Chuva (mm)',  weight='bold')
+axes[0].set_xlabel('Ano', weight='bold')
+axes[0].set_title('Acumulado Anual', weight='bold')
+axes[1].set_ylabel('Chuva (mm)', weight='bold')
+axes[1].set_xlabel('Mês', weight='bold')
+axes[1].set_title('Acumulado Mensal', weight='bold')
+
+plt.tight_layout()
+plt.show()
